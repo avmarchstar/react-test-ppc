be88144006d70cc7be7bc54e2ff36c7e53aa491a
avmarchstar <102725259+avmarchstar@users.noreply.github.com>
2025-06-13 13:46:46 +0000

    test2

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+const rootElement = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error('Элемент с id="root" не найден в index.html');
+}
