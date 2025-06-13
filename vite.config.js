5bc23621658d52532754ab526dde296237196002
avmarchstar <102725259+avmarchstar@users.noreply.github.com>
2025-06-13 13:49:46 +0000

    test2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // указывает корень проекта
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  server: {
+    open: true, // автоматически открывает браузер
+  },
 })
